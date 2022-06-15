c4756f39b599f7e04d39c9b33749f95543bb1d6d
MinJunKim21 <jinju8012@gmail.com>
2022-06-16 02:25:38 +0900

    testing

---
+++ nextjs-first-try/components/NavBar.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const NavBar = () => {
+  return <nav>
+    <Link href='/'>
+      <a>Home</a>
+    </Link>
+    <Link href='/about'>
+      <a>about</a>
+    </Link>
+  </nav>
+}
+
+export default NavBar
